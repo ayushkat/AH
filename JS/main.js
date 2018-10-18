27f941794cbfa97bf76a8d7e5f82338c50caeb57
ayushkat <43148357+ayushkat@users.noreply.github.com>
2018-10-18 13:52:23 -0400

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -3,4 +3,12 @@ function test1(){
 var fName = prompt("You got a name?");
 var myColour = prompt("Pick a color, " + fName);
 
+  if (myColour == "red"){ 
+    console.log("ha. this person likes red");
+}
+
+  else if(myColour == "green"){
+    console.log("ah he likes green. he's alright")
+}
+  
 }
